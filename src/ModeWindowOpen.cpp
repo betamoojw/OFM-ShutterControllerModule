029af213b1475f1e18ee1c496281709ff1c5e4dd
Michael Geramb <lasso_riefen0m@icloud.com>
2024-06-10 22:23:24 +0200

    implement window open mode (untested)

--- src/ModeWindowOpen.cpp
+++ src/ModeWindowOpen.cpp
@@ -6,18 +6,32 @@ const char *ModeWindowOpen::name()
 }
 void ModeWindowOpen::initGroupObjects()
 {
-
+    KoSHC_CHWindowOpenModeActive.value(false, DPT_Switch);
+    KoSHC_CHWindowOpenLockActive.value(false, DPT_Switch);
 }
 bool ModeWindowOpen::allowed(const CallContext& callContext)
 {
-    return false;
+    if (_recalcAllowed)
+    {
+        _allowed = KoSHC_CHWindow.value(DPT_OpenClose) && KoSHC_CHWindowOpenLockActive.value(DPT_Switch);
+    }
+    return _allowed;
 }
 void ModeWindowOpen::start()
 {
+    KoSHC_CHWindowOpenModeActive.value(true, DPT_Switch);
 }
 void ModeWindowOpen::stop()
 {
+    KoSHC_CHWindowOpenModeActive.value(false, DPT_Switch);
 }
 void ModeWindowOpen::processInputKo(GroupObject &ko)
 {
+    switch (ko.asap())
+    {
+        case SHC_KoCHWindow:
+        case SHC_KoCHWindowOpenLock:
+        _recalcAllowed = true;
+        break;
+    }
 }
