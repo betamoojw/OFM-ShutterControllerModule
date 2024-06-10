5b370fc20a4a7a92e470dec0d3e8297955f7b6e5
Michael Geramb <lasso_riefen0m@icloud.com>
2024-06-10 22:23:00 +0200

    Fix night mode lock behavior

--- src/ModeNight.cpp
+++ src/ModeNight.cpp
@@ -7,8 +7,9 @@ const char *ModeNight::name()
 void ModeNight::initGroupObjects()
 {
     KoSHC_CHModeNightActive.value(false, DPT_Switch);
+    KoSHC_CHModeNightLockActive.value(false, DPT_Switch);
 }
-bool ModeNight::allowed(const CallContext& callContext)
+bool ModeNight::allowed(const CallContext &callContext)
 {
     if (callContext.UtcDay != _lastDayUTC)
     {
@@ -23,117 +24,118 @@ bool ModeNight::allowed(const CallContext& callContext)
     {
         bool trigger = false;
         // 	<Enumeration Text="Kein Autstart" Value="0" Id="%ENID%" />
-		// 	<Enumeration Text="Uhrzeit" Value="1" Id="%ENID%" />
-		// 	<Enumeration Text="Sonne" Value="2" Id="%ENID%" />
-		// 	<Enumeration Text="Uhrzeit, Sonne (früheres Ergeignis)" Value="3" Id="%ENID%" />
-		// 	<Enumeration Text="Uhrzeit, Sonne (späteres Ergeignis)" Value="4" Id="%ENID%" />
+        // 	<Enumeration Text="Uhrzeit" Value="1" Id="%ENID%" />
+        // 	<Enumeration Text="Sonne" Value="2" Id="%ENID%" />
+        // 	<Enumeration Text="Uhrzeit, Sonne (früheres Ergeignis)" Value="3" Id="%ENID%" />
+        // 	<Enumeration Text="Uhrzeit, Sonne (späteres Ergeignis)" Value="4" Id="%ENID%" />
         switch (ParamSHC_ChannelNightModeStartBehavior)
         {
-            case 1:
-                if  (!startTime && callContext.minuteOfDay ==  knx.paramWord(SHC_ParamCalcIndex(SHC_ChannelModeNightFromTime)))
-                {
-                    startTime = true;
-                    trigger = true;
-                }
-                break;
-            case 2:
-                if (!sunRise && callContext.UtcHour < 720 && callContext.elevation >= getElevationFromSunRiseParameter())
-                {
-                    sunRise = true;
-                    trigger= true;
-                }
-                break;
-            case 3:
-                if  (!startTime && callContext.minuteOfDay == knx.paramWord(SHC_ParamCalcIndex(SHC_ChannelModeNightFromTime)))
-                {
-                    startTime = true;
-                    trigger = true;
-                }
-                if (!sunRise && callContext.UtcHour < 720 && callContext.elevation >= getElevationFromSunRiseParameter())
-                {
-                    sunRise = true;
-                    trigger = true;
-                }
-                break;
-            case 4:
-                if  (!startTime && callContext.minuteOfDay == knx.paramWord(SHC_ParamCalcIndex(SHC_ChannelModeNightFromTime)))
-                {
-                    startTime = true;
-                    trigger = sunRise;
-                }
-                if (!sunRise && callContext.UtcHour < 720 && callContext.elevation >= getElevationFromSunRiseParameter())
-                {
-                    sunRise = true;
-                    trigger = startTime;
-                }
-                break;
+        case 1:
+            if (!startTime && callContext.minuteOfDay == knx.paramWord(SHC_ParamCalcIndex(SHC_ChannelModeNightFromTime)))
+            {
+                startTime = true;
+                trigger = true;
+            }
+            break;
+        case 2:
+            if (!sunRise && callContext.UtcHour < 720 && callContext.elevation >= getElevationFromSunRiseParameter())
+            {
+                sunRise = true;
+                trigger = true;
+            }
+            break;
+        case 3:
+            if (!startTime && callContext.minuteOfDay == knx.paramWord(SHC_ParamCalcIndex(SHC_ChannelModeNightFromTime)))
+            {
+                startTime = true;
+                trigger = true;
+            }
+            if (!sunRise && callContext.UtcHour < 720 && callContext.elevation >= getElevationFromSunRiseParameter())
+            {
+                sunRise = true;
+                trigger = true;
+            }
+            break;
+        case 4:
+            if (!startTime && callContext.minuteOfDay == knx.paramWord(SHC_ParamCalcIndex(SHC_ChannelModeNightFromTime)))
+            {
+                startTime = true;
+                trigger = sunRise;
+            }
+            if (!sunRise && callContext.UtcHour < 720 && callContext.elevation >= getElevationFromSunRiseParameter())
+            {
+                sunRise = true;
+                trigger = startTime;
+            }
+            break;
         }
         if (trigger)
             _allowed = true;
-         // 	<Enumeration Text="Kein Autstart" Value="0" Id="%ENID%" />
-		// 	<Enumeration Text="Uhrzeit" Value="1" Id="%ENID%" />
-		// 	<Enumeration Text="Sonne" Value="2" Id="%ENID%" />
-		// 	<Enumeration Text="Uhrzeit, Sonne (früheres Ergeignis)" Value="3" Id="%ENID%" />
-		// 	<Enumeration Text="Uhrzeit, Sonne (späteres Ergeignis)" Value="4" Id="%ENID%" />
+        // 	<Enumeration Text="Kein Autstart" Value="0" Id="%ENID%" />
+        // 	<Enumeration Text="Uhrzeit" Value="1" Id="%ENID%" />
+        // 	<Enumeration Text="Sonne" Value="2" Id="%ENID%" />
+        // 	<Enumeration Text="Uhrzeit, Sonne (früheres Ergeignis)" Value="3" Id="%ENID%" />
+        // 	<Enumeration Text="Uhrzeit, Sonne (späteres Ergeignis)" Value="4" Id="%ENID%" />
         switch (ParamSHC_ChannelNightModeEndBehavior)
         {
-            case 1:
-                if  (!stopTime && callContext.minuteOfDay ==  knx.paramWord(SHC_ParamCalcIndex(SHC_ChannelModeNightToTime)))
-                {
-                    stopTime = true;
-                    trigger= true;
-                }
-                break;
-            case 2:
-                if (!sunSet && callContext.UtcHour > 720 && callContext.elevation <= getElevationFromSunSetParameter())
-                {
-                    sunSet = true;
-                    trigger = true;
-                }
-                break;
-            case 3:
-                if  (!stopTime && callContext.minuteOfDay == knx.paramWord(SHC_ParamCalcIndex(SHC_ChannelModeNightToTime)))
-                {
-                    stopTime = true;
-                    trigger = true;
-                }
-                if (!sunRise && callContext.UtcHour > 720 && callContext.elevation <= getElevationFromSunSetParameter())
-                {
-                    sunSet = true;
-                    trigger = true;
-                }
-                break;
-            case 4:
-                if  (!stopTime && callContext.minuteOfDay == knx.paramWord(SHC_ParamCalcIndex(SHC_ChannelModeNightToTime)))
-                {
-                    stopTime = true;
-                    trigger = sunRise;
-                }
-                if (!sunRise && callContext.UtcHour > 720 && callContext.elevation <= getElevationFromSunSetParameter())
-                {
-                    sunSet = true;
-                    trigger = stopTime;
-                }
-                break;
+        case 1:
+            if (!stopTime && callContext.minuteOfDay == knx.paramWord(SHC_ParamCalcIndex(SHC_ChannelModeNightToTime)))
+            {
+                stopTime = true;
+                trigger = true;
+            }
+            break;
+        case 2:
+            if (!sunSet && callContext.UtcHour > 720 && callContext.elevation <= getElevationFromSunSetParameter())
+            {
+                sunSet = true;
+                trigger = true;
+            }
+            break;
+        case 3:
+            if (!stopTime && callContext.minuteOfDay == knx.paramWord(SHC_ParamCalcIndex(SHC_ChannelModeNightToTime)))
+            {
+                stopTime = true;
+                trigger = true;
+            }
+            if (!sunRise && callContext.UtcHour > 720 && callContext.elevation <= getElevationFromSunSetParameter())
+            {
+                sunSet = true;
+                trigger = true;
+            }
+            break;
+        case 4:
+            if (!stopTime && callContext.minuteOfDay == knx.paramWord(SHC_ParamCalcIndex(SHC_ChannelModeNightToTime)))
+            {
+                stopTime = true;
+                trigger = sunRise;
+            }
+            if (!sunRise && callContext.UtcHour > 720 && callContext.elevation <= getElevationFromSunSetParameter())
+            {
+                sunSet = true;
+                trigger = stopTime;
+            }
+            break;
         }
         if (trigger)
             _allowed = false;
-        
     }
+    if (KoSHC_CHModeNightLockActive.value(DPT_Switch))
+        return false;
     return _allowed;
 }
 
 // <Enumeration Text="vor Sonnenuntergang" Value="0" Id="%ENID%" />
 // <Enumeration Text="bei Sonnenuntergang" Value="1" Id="%ENID%" />
-// <Enumeration Text="nach Sonnenuntergang" Value="2" Id="%ENID%" />				
+// <Enumeration Text="nach Sonnenuntergang" Value="2" Id="%ENID%" />
 double ModeNight::getElevationFromSunSetParameter()
 {
     switch (ParamSHC_ChannelModeNightSunSet)
     {
     case 0:
-        return ParamSHC_ChannelModeNightSunSetElevationOffset;    
+        return ParamSHC_ChannelModeNightSunSetElevationOffset;
     case 2:
-        return ((double) ParamSHC_ChannelModeNightSunSetElevationOffset) * -1;    
+        return ((double)ParamSHC_ChannelModeNightSunSetElevationOffset) * -1;
     default:
         return 0;
     }
@@ -147,30 +149,34 @@ double ModeNight::getElevationFromSunRiseParameter()
     switch (ParamSHC_ChannelModeNightSunRise)
     {
     case 0:
-        return ((double) ParamSHC_ChannelModeNightSunRiseElevationOffset) * -1;    
+        return ((double)ParamSHC_ChannelModeNightSunRiseElevationOffset) * -1;
     case 2:
-        return ParamSHC_ChannelModeNightSunRiseElevationOffset;    
+        return ParamSHC_ChannelModeNightSunRiseElevationOffset;
     default:
         return 0;
     }
 }
 
-
 void ModeNight::start()
 {
+    KoSHC_CHModeNightActive.value(true, DPT_Switch);
 }
 void ModeNight::stop()
 {
+    KoSHC_CHModeNightActive.value(false, DPT_Switch);
 }
+
 void ModeNight::processInputKo(GroupObject &ko)
 {
-   switch (ko.asap())
-   {
-   case SHC_KoCHModeNight:
-    _allowed = ko.value(DPT_Switch);
-    break;
-   
-   default:
-    break;
-   }
+    switch (ko.asap())
+    {
+    case SHC_KoCHModeNight:
+        _allowed = ko.value(DPT_Switch);
+        break;
+    case SHC_KoCHModeNightLock:
+        KoSHC_CHModeNightLockActive.value(ko.value(DPT_Switch), DPT_Switch);
+        break;
+    default:
+        break;
+    }
 }
